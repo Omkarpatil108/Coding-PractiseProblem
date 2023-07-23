5ee65bfab44c06f8b02ca6345fff53f5d95b28fc
Omkar Patil <91424350+Omkarpatil108@users.noreply.github.com>
2023-07-23 22:47:22 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Trapping Rain Water/trapping-rain-water.cpp
@@ -0,0 +1,75 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+
+    // Function to find the trapped water between the blocks.
+    public:
+    long long trappingWater(int arr[], int n){
+        // code here
+        int l=0;
+        int r=n-1;
+        long long int res=0;
+        int leftmost=0,rightmost=0;
+        while(l<r)
+        {
+            if(arr[l]<=arr[r])
+            {
+                if(arr[l]>=leftmost)
+                {
+                    leftmost=arr[l];
+                }
+                else{
+                    res+=(leftmost-arr[l]);
+                }
+                l++;
+            }
+            else
+            {
+                if(arr[r]>=rightmost)
+                {
+                    rightmost=arr[r];
+                }
+                else{
+                    res+=(rightmost-arr[r]);
+                }
+                r--;
+            }
+        }
+        return res;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    
+    int t;
+    //testcases
+    cin >> t;
+    
+    while(t--){
+        int n;
+        
+        //size of array
+        cin >> n;
+        
+        int a[n];
+        
+        //adding elements to the array
+        for(int i =0;i<n;i++){
+            cin >> a[i];            
+        }
+        Solution obj;
+        //calling trappingWater() function
+        cout << obj.trappingWater(a, n) << endl;
+        
+    }
+    
+    return 0;
+}
+// } Driver Code Ends
