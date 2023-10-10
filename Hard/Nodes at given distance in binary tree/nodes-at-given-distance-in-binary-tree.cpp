9f1afe339c33cfa0a2a474d392c959f40f716c16
Omkar Patil <91424350+Omkarpatil108@users.noreply.github.com>
2023-10-10 22:40:11 +0530

    Added Solution - GfG to GitHub

---
+++ Hard/Nodes at given distance in binary tree/nodes-at-given-distance-in-binary-tree.cpp
@@ -0,0 +1,173 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// Tree Node
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+};
+
+// Utility function to create a new Tree Node
+Node* newNode(int val)
+{
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+    
+    return temp;
+}
+
+// Function to Build Tree
+Node* buildTree(string str)
+{   
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+            return NULL;
+    
+    // Creating vector of strings from input 
+    // string after spliting by space
+    vector<string> ip;
+    
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+        
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+        
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+        
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+            
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+            
+        // Get the current node's value from the string
+        string currVal = ip[i];
+            
+        // If the left child is not null
+        if(currVal != "N") {
+                
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+            
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+            
+        // If the right child is not null
+        if(currVal != "N") {
+                
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+    
+    return root;
+}
+
+
+// } Driver Code Ends
+/* A binary Tree node
+struct Node
+{
+    int data;
+    struct Node *left, *right;
+};
+*/
+
+class Solution
+{
+private:
+map<Node*, Node*>parent;
+    Node* tar;
+    vector<int>ans;
+
+public:
+void findparent(Node* root, int target, Node* p){
+        if(root == nullptr) return;
+        parent[root] = p;
+        if(root->data == target) tar = root;
+        findparent(root->left, target, root);
+        findparent(root->right, target, root);
+    }
+     
+    void solve(Node* node, int dis, Node* prev){
+        if(node == nullptr) return;
+        if(dis == 0){
+            ans.push_back(node->data);
+            return;
+        }
+        if(node->left != prev)
+        solve(node->left, dis-1, node);
+        
+        if(node->right != prev)
+        solve(node->right, dis-1, node);
+        
+        if(parent[node] != prev)
+        solve(parent[node], dis-1, node);
+    }
+
+    vector <int> KDistanceNodes(Node* root, int target , int k)
+    {
+        // return the sorted vector of all nodes at k dist
+         parent.clear();
+        ans.clear();
+        findparent(root , target, nullptr);
+       
+        solve(tar, k, nullptr);
+        sort(ans.begin(), ans.end());
+        return ans;
+    
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    getchar();
+    
+    Solution x = Solution();
+    
+    while(t--)
+    {
+        string s;
+        getline(cin,s);
+        Node* head = buildTree(s);
+        
+        int target, k;
+        cin>> target >> k;
+        getchar();
+        
+        vector <int> res = x.KDistanceNodes(head, target, k);
+        
+        for( int i=0; i<res.size(); i++ )
+            cout<< res[i] << " ";
+        cout<<endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
